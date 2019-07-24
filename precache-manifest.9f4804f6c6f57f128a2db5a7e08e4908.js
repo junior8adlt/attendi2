b79f9db722d7cb0a3e488fbcad0eb4cca0531ea4
Alberto Ochoa <alberto.ochoadltorre@gmail.com>
2019-07-23 23:13:30 -0500

    Updates

---
+++ precache-manifest.9f4804f6c6f57f128a2db5a7e08e4908.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf10bdf128ad95577096ddbe8fbccd18",
+    "url": "/attendi2/index.html"
+  },
+  {
+    "revision": "8f31817ad2f95a40297e",
+    "url": "/attendi2/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b68179e419e5d2f9def5",
+    "url": "/attendi2/static/css/main.dde3d4fe.chunk.css"
+  },
+  {
+    "revision": "8f31817ad2f95a40297e",
+    "url": "/attendi2/static/js/2.384b2fd0.chunk.js"
+  },
+  {
+    "revision": "b68179e419e5d2f9def5",
+    "url": "/attendi2/static/js/main.b20ae985.chunk.js"
+  },
+  {
+    "revision": "4c41e1edb1df3dbbccbe",
+    "url": "/attendi2/static/js/runtime~main.1d2e659c.js"
+  },
+  {
+    "revision": "b7532bcfc90422f1e1cb7d9311f4da5f",
+    "url": "/attendi2/static/media/Logo Attendi_movil.b7532bcf.png"
+  },
+  {
+    "revision": "037ef6c216bd53bcaf72419b7fb9f37e",
+    "url": "/attendi2/static/media/escala_1_mov.037ef6c2.png"
+  },
+  {
+    "revision": "f8a779fbc01a207915da71c85b038754",
+    "url": "/attendi2/static/media/escala_3_mov.f8a779fb.png"
+  },
+  {
+    "revision": "ab9f9ea3d7e0d759c7bd3687e1e4bfcc",
+    "url": "/attendi2/static/media/escala_5_mov.ab9f9ea3.png"
+  },
+  {
+    "revision": "4b1fcf2c5c3d2a3503fe799a11a2754e",
+    "url": "/attendi2/static/media/escala_6_mov.4b1fcf2c.png"
+  },
+  {
+    "revision": "070a9269082474ecd06b6d64ebd0fa2f",
+    "url": "/attendi2/static/media/glacial-indifference.bold.070a9269.otf"
+  },
+  {
+    "revision": "008080d5594fd00507fc8a2c93443d39",
+    "url": "/attendi2/static/media/glacial-indifference.regular.008080d5.otf"
+  }
+]);
